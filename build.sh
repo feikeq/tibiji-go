2e92a6f836417e1aebdee1f4a886885b6c300991
feikeq <feikeq@163.com>
2023-06-30 14:31:29 +0800

    后台启动并使其常驻内存

--- build.sh
+++ build.sh
@@ -20,6 +20,11 @@ rm ./dist/config/*.go
 # 移除调试信息 -ldflags 调试信息会增加可执行文件的大小您可以使用以下命令编译可执行文件时移除调试信息：
 # go build -ldflags "-w -s -extldflags '-static'" -o ./dist/ . # 使用Go的静态编译。静态编译会将所有依赖库打包到可执行文件中，从而减小文件大小
 
-# 交叉编译到Windows和Linux平台分别编译可执行文件：
+
+# 交叉编译到Windows和Linux和Mac全平台分别编译可执行文件：
+# 编译到 windows ( 后台启动并使其常驻内存 "start /b tibiji-go.exe")
 GOOS=windows GOARCH=amd64 go build -o ./dist/ .
+# 编译到 Linux ( 后台启动并使其常驻内存 "nohup ./tibiji-go &")
 GOOS=linux GOARCH=amd64 go build -o ./dist/ .
+# 编译到 macOS ( 后台启动并使其常驻内存 "nohup ./tibiji-go.mac &")
+GOOS=darwin GOARCH=amd64 go build -o ./dist/tibiji-go.mac .
